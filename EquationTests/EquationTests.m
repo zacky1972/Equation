3b713ef9e6ccbd87a7d11457dab86f0ad8a0e79f
Susumu Yamazaki <zacky1972@gmail.com>
2012-12-12 13:36:16 +0900

    (2-1) ソフトウェアテストの作成 コメントを足しました。

--- EquationTests/EquationTests.m
+++ EquationTests/EquationTests.m
@@ -25,9 +25,15 @@ Equation* e;
     [super tearDown];
 }
 
+// x^2 - 3x + 2 = 0 をテスト
+// 実数解 2つの場合。
 - (void)test1
 {
     e = [[Equation alloc] initWithA:1 b:-3 c:2];
+    
+    // STAssertEqualsWithAccuracy は浮動小数点演算をテストするときに使います。
+    // 浮動小数点演算では誤差が発生するので，誤差の範囲内で等しいことを確かめる必要があります。
+    // accuracy とは誤差のことです。
     STAssertEqualsWithAccuracy(2.0, [e result1], 0.00001, @"result1 error");
     STAssertEqualsWithAccuracy(1.0, [e result2], 0.00001, @"result2 error");
 }
